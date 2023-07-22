7517123bc78f2e7f49cf72d0212537aae586a535
Daniel Fidalgo <daniel.fidalgo@gmail.com>
2023-07-22 14:54:40 -0500

    main - adding files

---
+++ src/main/kotlin/api/impl/UserApiImpl.kt
@@ -0,0 +1,23 @@
+package api.impl
+
+import api.UserApi
+import api.dto.`in`.UserCreate
+import api.dto.`in`.UserUpdate
+import api.dto.out.User
+import domain.users.UserService
+import org.koin.core.annotation.Single
+
+@Single
+class UserApiImpl(private val userService: UserService): UserApi {
+    override fun createUser(dto: UserCreate): String {
+       return userService.insertUser(dto.toDomain())
+    }
+
+    override fun updateUser(dto: UserUpdate) {
+        userService.updateUser(dto.toDomain())
+    }
+
+    override fun getUser(id: String): User {
+        return  User.fromDomain(userService.getUserById(id))
+    }
+}
